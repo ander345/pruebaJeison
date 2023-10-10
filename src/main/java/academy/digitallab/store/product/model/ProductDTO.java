393ee26c68bde41202b71e5b0c49535b7411311e
jandersoncasas <jandersoncasas@gmail.com>
2023-10-10 23:00:47 +0200

    Create ProductDTO.java

---
+++ src/main/java/academy/digitallab/store/product/model/ProductDTO.java
@@ -0,0 +1,15 @@
+package academy.digitallab.store.product.model;
+
+import java.util.Date;
+
+
+public class ProductDTO {
+
+	private Long id;
+	private String name;
+	private String description;
+	private Double stock;
+	private Double price;
+	private String status;
+	private Date createAt;
+}
